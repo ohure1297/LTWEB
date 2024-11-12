1689ff8fe58cc9d72e35e32b503da81fa861199a
ductin <nguyenductin011011@gmail.com>
2024-11-12 11:47:11 +0700

    change logo

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import { Link, Routes, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import Small_Logo from './utils/img/Logo.png';
 import Navbar from 'react-bootstrap/Navbar';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
+import Reserve from './pages/Reserve';
 import About from './pages/About';
 import Contact from './pages/Contact';
 
@@ -15,16 +17,18 @@ function App() {
         <Container>
           <Navbar.Brand>
             <Link to="/" className='navbar-brand text-success fw-semibold'>
-              React Restaurant
+              <img src={Small_Logo} alt="small_logo"/>
             </Link>
           </Navbar.Brand>
+          <h1 className=''>Hello</h1>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto justify-content-end w-100'>
-              <Nav.Link href='/' className='active text-uppercase'>Home</Nav.Link>
-              <Nav.Link href='/menu' className='text-uppercase'>Menu</Nav.Link>
-              <Nav.Link href='/about' className='text-uppercase'>About</Nav.Link>
-              <Nav.Link href='/contact' className='text-uppercase'>Contact</Nav.Link>
+              {/* <Nav.Link href='/' className='active text-uppercase'>ABOUT US</Nav.Link> */}
+              <Nav.Link href='/about' className='active text-uppercase'>ABOUT US</Nav.Link>
+              <Nav.Link href='/menu' className='text-uppercase'>MENUS</Nav.Link>
+              <Nav.Link href='/reserve' className='text-uppercase'>RESERVE</Nav.Link>
+              <Nav.Link href='/contact' className='text-uppercase'>CONTACT</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -32,8 +36,9 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/menu' element={<Menu />} />
         <Route path='/about' element={<About />} />
+        <Route path='/menu' element={<Menu/>} />
+        <Route path='/reserve' element={<Reserve />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
 
